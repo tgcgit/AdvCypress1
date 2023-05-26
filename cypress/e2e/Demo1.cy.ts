067a21e3b739103e7235c65a4a9d7413d340a71d
Trainer <trainer@example.com>
2023-05-26 19:53:15 +0530

    Added changes to trigger jenkins

--- cypress/e2e/Demo1.cy.ts
+++ cypress/e2e/Demo1.cy.ts
@@ -6,7 +6,7 @@ describe('Demo1_TestSuite', () => {
 it('Demo_TestCase1', () => {
   cy.visit('https://blazedemo.com')
   cy.title().should('contain', 'BlazeDemo')
-
+  //----- added this line to trigger Jenkins job
 })
 
 })
